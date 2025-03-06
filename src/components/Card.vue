a08958cb3dede9d9fa92d49342eeec6282b0218a
newkub <newkubise@gmail.com>
2025-03-06 22:48:19 +0700

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="flex items-start space-x-4 opacity-80 hover:opacity-100 transition-opacity">
+  <div :class="`text-${color}-500 mt-1`">
+    <slot name="icon"></slot>
+  </div>
+  <div>
+    <h2 class="text-xl font-semibold">{{ title }}</h2>
+    <p class="text-gray-400">{{ description }}</p>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+interface CardProps {
+  title: string
+  description: string
+  color?: string
+}
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: 'gray'
+    }
+  }
+}
+</script>
