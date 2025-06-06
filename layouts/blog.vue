dfe36179c5427349f4bea3827c596c1726f8ecdc
newkub <newkubise@gmail.com>
2025-06-07 01:10:48 +0700

    feat(layouts): add default layout

---
+++ layouts/blog.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const showSidebar = ref(true);
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <slot name="header" />
+    
+    <div class="flex flex-1">
+      <aside 
+        v-if="showSidebar" 
+        class="w-64 border-r p-4"
+      >
+        <slot name="sidebar" />
+      </aside>
+      
+      <main class="flex-1 p-4">
+        <slot />
+      </main>
+    </div>
+    
+    <slot name="footer" />
+  </div>
+</template>
+
+<style>
+/* Styles will be handled by UnoCSS */
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <slot name="header" />
+    
+    <main class="flex-1 p-4">
+      <slot />
+    </main>
+    
+    <slot name="footer" />
+  </div>
+</template>
+
+<style>
+/* Styles will be handled by UnoCSS */
+</style>
