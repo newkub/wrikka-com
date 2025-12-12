bb613ab1d99c4701001b37cae34a83322cc1ba11
Veerapong <newkubise@gmail.com>
2025-12-12 17:48:01 +0700

    chore(config): update nuxt and tsconfig configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				srcDir: "app/",
-				compatibilityDate: "2025-05-15",
-				devtools: { enabled: false },
-				modules: [
-								"@unocss/nuxt",
-								"@vueuse/nuxt",
-								"@pinia/nuxt",
-								"@nuxt/content",
-				],
-				nitro: {
-								preset: "cloudflare",
-				},
-				content: {
-								database: {
-												driver: 'fs'
-								}
-				},
+	srcDir: "app/",
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: false },
+	modules: [
+		"@unocss/nuxt",
+		"@vueuse/nuxt",
+		"@pinia/nuxt",
+		"@nuxt/content",
+	],
+	nitro: {
+		preset: "cloudflare",
+	},
+	content: {
+		database: {
+			driver: "fs",
+		},
+	},
 });
