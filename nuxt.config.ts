eed7fdf6c4ca0b3811f27e5a38d2414ad17f22f3
Veerapong <newkubise@gmail.com>
2026-01-08 05:30:19 +0700

    fix: remove markdown.css import temporarily

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-	css: ["~/assets/css/markdown.css", "~/assets/css/theme.css", "~/assets/css/responsive.css"],
+	css: ["~/assets/css/theme.css", "~/assets/css/responsive.css"],
 	app: {
 		head: {
 			htmlAttrs: {
